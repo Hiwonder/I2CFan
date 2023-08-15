0812ddafb17dffcd02e19fde8cc62939a2f3fdf1
tiandiyishaou <support@hiwonder.com>
2023-08-15 11:16:51 +0800

    blocks

---
+++ README.md
@@ -0,0 +1,14 @@
+# I2CFan
+ Blocks for Hiwonder I2CFan module
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/i2cfan
+(The metadata above is needed for package search.)
+
+```package
+i2cfan=github:Hiwonder/I2CFan
+```
---
+++ main.ts
@@ -0,0 +1,26 @@
+/*
+ i2cfan package
+*/
+//% weight=10 icon="\uf140" color=#2896ff
+namespace i2cfan {
+
+    const I2C_ADDR = 0x39
+
+    const SET_MODE = 0
+
+    function i2cwrite(adress: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = value;
+        pins.i2cWriteBuffer(adress, buf);
+    }
+
+    /**
+    * Set the fan speed
+    */
+    //% weight=98 blockId=fanSpeed block="Set the iic_fan speed %speed"
+    //% speed.min=-100 speed.max=100
+    export function fanSpeed(speed: number) {
+        i2cwrite(I2C_ADDR, SET_MODE, speed)
+    }
+}
